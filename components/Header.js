d86ff343b7d3bfbfbc25a9d62796fb8f8fa78eb9
Chris <mr.chrisnash@gmail.com>
2021-10-19 08:33:44 -0700

    sign in auth with google account now working

--- components/Header.js
+++ components/Header.js
@@ -11,22 +11,18 @@ import {
 } from '@heroicons/react/outline';
 import { useRouter } from "next/dist/client/router";
 import { useRecoilState } from "recoil";
-import { useSession } from "next-auth/react";
+import { useSession, signIn, signOut } from "next-auth/react";
 //import { modalState } from '../atoms/modalAtom';
 
 function Header() {
+  //rename data object as 'session'
   const { data: session } = useSession();
+  
   //const [open, setOpen] = useRecoilState(modalState); // global state using recoil. see atoms/modalAtom.js
   const router = useRouter();
 
   console.log(session);
 
-
-  async function signIn() {
-  }
-
-  function signOut() {
-  }
 
 
   return (
@@ -55,22 +51,30 @@ function Header() {
       </div>
 
       {/* RIGHT - icons/signin */}
+
       <div className='space-x-1 flex text-center'>
         <MenuIcon className='h-10 w-10 md:hidden' />
-        
         <HomeIcon className='navBtn' />
-        <PlusCircleIcon className='navBtn' />
-        {/* <PaperAirplaneIcon className='navBtn rotate-45 ' /> */}
-        {/* <UserGroupIcon className='navBtn' /> */}
-        {/* <UserCircleIcon className='navBtn' /> */}
-        <button onClick={signIn}>Sign In</button>
-        <button onClick={signOut}>Sign Out</button>
 
+        {session ? (
+          <>
+            <PlusCircleIcon onClick={() => setOpen(true)} className='navBtn' />
+            <img 
+              src={session?.user?.image} 
+              onClick={signOut}
+              alt='profile pic' 
+              className='h-8 w-8 rounded-full hover:scale-125 cursor-pointer '
+            />
+            <button onClick={signOut} className='hidden xl:inline-block hover:scale-110'>Sign Out</button>
+          </>
+        ) : (
+          <>
+            <UserCircleIcon className='navBtn' onClick={signIn} />
+            <button className='hover:scale-110' onClick={signIn}>Sign In</button>
+          </>
+        )}
 
       </div>
-
-
-
     </div>
   )
 }
