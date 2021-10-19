3e127458df928c97022e4b12c264ab57aedc03ed
Chris <mr.chrisnash@gmail.com>
2021-10-19 09:59:31 -0700

    added session changes

--- components/Stories.js
+++ components/Stories.js
@@ -1,9 +1,11 @@
 import faker from 'faker';
 import { useEffect, useState } from 'react';
 import Story from './Story';
+import { useSession } from 'next-auth/react';
 
 function Stories() {
   const [stories, setStories] = useState([]);
+  const { data: session } = useSession();
 
   useEffect(() => {
     const suggestions = [...Array(14)].map((i) => ({
@@ -18,14 +20,23 @@ function Stories() {
     <div className='flex flex-row overflow-x-auto 
     scrollbar-thin scrollbar-thumb-green-600 scrollbar-track-yellow-200 p-2'>
     
+      {/* if there is a session, show users info as the first story */}
+      {session && (
+        <Story 
+          key={session.user.id}
+          image={session.user.image}
+          username={session.user.username}
+        />
+      )}
+
       {stories.map(story => {
         return(
           <Story 
+            key={story.id}
             username={story.username}
             image={story.avatar}
           />
         )
-
       })}
 
     </div>
