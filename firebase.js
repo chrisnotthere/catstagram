feac2d1ada90eafeeb8f3bf9828d66df8d4d0b3a
Chris <mr.chrisnash@gmail.com>
2021-10-19 17:10:44 -0700

    config

--- firebase.js
+++ firebase.js
@@ -15,6 +15,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+//const app = initializeApp(firebaseConfig);
 const db = getFirestore();
 const storage = getStorage();
 
