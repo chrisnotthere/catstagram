30c35cd158285ec73523dd974497ddc69c91050b
Chris <mr.chrisnash@gmail.com>
2021-10-20 16:48:46 -0700

    miniprofile responds to guest user login

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -1,34 +1,71 @@
-import { useSession, signIn, signOut } from 'next-auth/react'
+import { useSession, signIn, signOut as signOutSession } from 'next-auth/react'
 import { useAuth } from '../pages/auth/authUserContext';
+import { QuestionMarkCircleIcon } from '@heroicons/react/outline';
 
 function MiniProfile() {
 
   const { data: session } = useSession();
-  const { authUser, loading } = useAuth();
-
-  console.log({authUser}, {loading});
+  const { authUser, loading, signOut} = useAuth();
+  
 
   return (
-    <div className='flex items-center mt-14 ml-10'>
-      <img
-        className='w-16 h-16 rounded-full border border-green-600 p-[2px] '
-        src={session?.user.image}
-        alt=''
-      />
+    <>
 
-      <div className='flex-1 mx-4'>
-      <h2 className='font-bold'>{session?.user?.username}</h2>
-        <h3 className='text-sm text-green-600 font-semibold'>Welcome to Catstagram!</h3>
-      </div>
+      {session && (
+        
+        <div className='flex items-center mt-14 ml-10'>
+          <img
+            className='w-16 h-16 rounded-full border border-green-600 p-[2px] '
+            src={session?.user.image}
+            alt=''
+          />
+    
+          <div className='flex-1 mx-4'>
+          <h2 className='font-bold'>{session?.user?.username}</h2>
+            <h3 className='text-sm text-green-600 font-semibold'>Welcome to Catstagram!</h3>
+          </div>
 
-      <button 
-        onClick={signOut}
-        className='text-blue-500 text-sm font-semibold'
-      >
-        Sign Out
-      </button>
+        <button 
+          onClick={signOutSession}
+          className='text-blue-500 text-sm font-semibold'
+        >
+          Sign Out
+        </button>
 
-    </div>
+
+        </div>
+      )}
+
+      {authUser && (
+
+        <div className='flex items-center mt-14 ml-10'>
+          {/* <img
+            className='w-16 h-16 rounded-full border border-green-600 p-[2px] '
+            src={session?.user.image}
+            alt=''
+          /> */}
+          <QuestionMarkCircleIcon 
+            onClick={signOut} 
+            className='h-16 w-16 rounded-full border border-green-600 p-[2px] '
+          />
+
+          <div className='flex-1 mx-4'>
+          <h2 className='font-bold'>Guest Account</h2>
+            <h3 className='text-sm text-green-600 font-semibold'>Welcome to Catstagram!</h3>
+          </div>
+
+          <button 
+            onClick={signOut}
+            className='text-blue-500 text-sm font-semibold'
+          >
+            Sign Out
+          </button>
+
+        </div>
+      )}
+
+
+    </>
   )
 }
 
